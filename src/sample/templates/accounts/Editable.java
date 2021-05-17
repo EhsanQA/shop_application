49fa8695a413b2418c838ba6311a9fc7b1e29dda
hooman <hoomankhalili43@gmail.com>
2021-05-17 21:50:53 +0430

    new

---
+++ src/sample/templates/accounts/Editable.java
@@ -0,0 +1,5 @@
+package sample.templates.accounts;
+
+public interface Editable {
+    String addStock(int productId, int number);
+}
---
+++ src/sample/templates/accounts/Loginable.java
@@ -0,0 +1,5 @@
+package sample.templates.accounts;
+
+public interface Loginable {
+    boolean login(String email, String password);
+}
